93aaa3cdf63922cffd54cea43f0801f1e5845246
Pavel Hunko <pavelhunko@gmail.com>
2014-10-03 02:28:18 +0300

    Login page implemented. not sure, whether it's working or not

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,43 @@
 package controllers;
 
 import model.Contact;
+import model.User;
 import play.*;
+import play.data.*;
 import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render(Contact.find.all()));
-    }
+	public static Result index() {
+		return ok(index.render(Contact.find.all()));
+	}
+
+	public static Result login() {
+		return ok(login.render(Form.form(Login.class)));
+	}
+
+	public static class Login {
+		public String email;
+		public String password;
+		
+		public String validate() {
+			if (User.authenticate(email, password) == null) {
+				return "Invalid user or password";
+			}
+			return null;
+		}
+	}
+
+	public static Result authenticate() {
+		Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+			return badRequest(login.render(loginForm));
+		} else {
+			session().clear();
+			session("email", loginForm.get().email);
+			return redirect(routes.Application.index());
+		}
+	}
 
 }
